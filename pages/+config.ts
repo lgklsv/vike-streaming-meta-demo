4bef499e39d9e6f181e187d49f9d1fc035cc728a
lgklsv <kolosov20011@gmail.com>
2025-04-09 18:37:46 +0400

    🚧 Vike base assets test

--- pages/+config.ts
+++ pages/+config.ts
@@ -7,6 +7,12 @@ import vikeServer from "vike-server/config";
 // Default config (can be overridden by pages)
 // https://vike.dev/config
 
+const isProduction = process.env.NODE_ENV === "production";
+
+const baseAssets = isProduction ? "https://cdn.example.org/my-website-assets/" : undefined;
+
+console.log("BASE_ASSETS", baseAssets);
+
 export default {
   // https://vike.dev/Layout
   Layout,
@@ -15,6 +21,7 @@ export default {
   title: "My Vike App",
   description: "Demo showcasing Vike",
   stream: "web",
+  baseAssets,
 
   server: "server/index.js",
   extends: [vikeServer, vikeReact, vikeReactQuery],
